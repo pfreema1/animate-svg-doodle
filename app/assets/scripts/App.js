483133ac620e93d84e2016683f69bef926060ffe
Paul <pfreema1@gmail.com>
2018-04-10 17:15:23 -0400

    scaled up svg

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,7 @@
-console.log("ᕕ( ᐛ )ᕗ -- weeeeeee!");
+const circlesArr = document.querySelectorAll("path");
+
+console.log(circlesArr);
+
+setTimeout(() => {
+  circlesArr[1].classList.add("grow");
+}, 1000);
