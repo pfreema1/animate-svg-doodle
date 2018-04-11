ace81ce007dfb16e132d240fd7526dd9d4fc557a
pfreema1 <pfreema1@gmail.com>
2018-04-11 10:31:24 -0400

    added bezier curves with control points

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -17,11 +17,11 @@ const getRandomInt = (min, max) => {
 
 const moveXWithinBounds = (x, size) => {
   if(x < size) {
-    // element is overflowing off-screen to the left 
-    return size / 2;
-  } else if(x > WINDOW_HEIGHT - size) {
-    // element is overflowing off-screen to the right
-    return WINDOW_HEIGHT - size;
+  // element is overflowing off-screen to the left 
+    return size;
+  } else if(x > WINDOW_WIDTH - size) {
+  // element is overflowing off-screen to the right
+    return WINDOW_WIDTH - size;
   }
   else {
     return x;
@@ -31,10 +31,10 @@ const moveXWithinBounds = (x, size) => {
 const moveYWithinBounds = (y, size) => {
   if(y < size) {
     // element is overflowing off-screen to the left 
-    return size / 2;
-  } else if(y > WINDOW_WIDTH - size) {
+    return size;
+  } else if(y > WINDOW_HEIGHT - size) {
     // element is overflowing off-screen to the right
-    return WINDOW_WIDTH - size;
+    return WINDOW_HEIGHT - size;
   }
   else {
     return y;
@@ -67,15 +67,98 @@ const moveYWithinBounds = (y, size) => {
 // c.stroke();
 
 
-c.strokeStyle = 'blue';
-for(let i = 0; i < 200; i++) {
-  c.beginPath();
+// c.strokeStyle = 'blue';
+// for(let i = 0; i < 200; i++) {
+//   c.beginPath();
   
-  let size = getRandomInt(1, 200);
-  let x = getRandomInt(0, WINDOW_WIDTH);
-  let x = moveXWithinBounds(getRandomInt(0, WINDOW_WIDTH), size);
-  let y = moveYWithinBounds(getRandomInt(0, WINDOW_HEIGHT), size);
+//   let size = getRandomInt(1, 50);
+//   let x = moveXWithinBounds(getRandomInt(0, WINDOW_WIDTH), size);
+//   let y = moveYWithinBounds(getRandomInt(0, WINDOW_HEIGHT), size);
 
-  c.arc(x, y, size, 0, 2 * Math.PI, false);
-  c.stroke();
+//   c.arc(x, y, size, 0, 2 * Math.PI, false);
+//   c.stroke();
+// }
+
+const drawBoundedRandomPosCircles = (amountCircles, sizeMin, sizeMax) => {
+  for(let i = 0; i < amountCircles; i++) {
+    c.beginPath();
+
+    let size = getRandomInt(sizeMin, sizeMax);
+    let x = moveXWithinBounds(getRandomInt(0, WINDOW_WIDTH), size);
+    let y = moveYWithinBounds(getRandomInt(0, WINDOW_HEIGHT), size);
+
+    c.arc(x, y, size, 0, 2 * Math.PI, false);
+    c.stroke();
+  }
 }
+
+const drawBoundedRandomPosSquares = (amountSquares, sizeMin, sizeMax) => {
+  for(let i = 0; i < amountSquares; i++) {
+    c.beginPath();
+
+    let size = getRandomInt(sizeMin, sizeMax);
+    let x = moveXWithinBounds(getRandomInt(0, WINDOW_WIDTH), size);
+    let y = moveYWithinBounds(getRandomInt(0, WINDOW_HEIGHT), size);
+
+    c.fillRect(x, y, size, size);
+    c.stroke();
+  }
+}
+
+const drawBoundedRandomBezierCurves = (numPoints, drawPoints = false, connected = false) => {
+  for(let i = 0; i < numPoints; i++) {
+    c.beginPath();
+
+    let startPoint = {
+      x: moveXWithinBounds(getRandomInt(0, WINDOW_WIDTH), 10),
+      y: moveYWithinBounds(getRandomInt(0, WINDOW_HEIGHT), 10)
+    };
+    let controlPoint1 = {
+      x: moveXWithinBounds(getRandomInt(0, WINDOW_WIDTH), 10),
+      y: moveYWithinBounds(getRandomInt(0, WINDOW_HEIGHT), 10)
+    };
+    let controlPoint2 = {
+      x: moveXWithinBounds(getRandomInt(0, WINDOW_WIDTH), 10),
+      y: moveYWithinBounds(getRandomInt(0, WINDOW_HEIGHT), 10)
+    };
+    let endPoint = {
+      x: moveXWithinBounds(getRandomInt(0, WINDOW_WIDTH), 10),
+      y: moveYWithinBounds(getRandomInt(0, WINDOW_HEIGHT), 10)
+    };
+    c.moveTo(startPoint.x, startPoint.y);
+    c.bezierCurveTo(
+      controlPoint1.x,
+      controlPoint1.y,
+      controlPoint2.x,
+      controlPoint2.y,
+      endPoint.x,
+      endPoint.y
+    );
+
+    if(drawPoints) {
+      c.fillStyle = 'blue';
+      // start point
+      c.fillRect(startPoint.x, startPoint.y, 10, 10);
+      // end point
+      c.fillRect(endPoint.x, endPoint.y, 10, 10);
+
+      c.fillStyle = 'red';
+      // control points
+      c.fillRect(controlPoint1.x, controlPoint1.y, 10, 10);
+      c.fillRect(controlPoint2.x, controlPoint2.y, 10, 10);
+      
+
+    }
+    
+    c.stroke();
+  }
+}
+
+// start
+(function() {
+  c.strokeStyle = 'purple';
+
+  // drawBoundedRandomPosCircles(50, 2, 10);
+  // drawBoundedRandomPosSquares(100, 2, 60);
+  drawBoundedRandomBezierCurves(1, true, false);
+})();
