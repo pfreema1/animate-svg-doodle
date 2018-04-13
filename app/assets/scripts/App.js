6c3f0ac95a9502ec7accfdaa125c762d2d20901d
Paul <pfreema1@gmail.com>
2018-04-13 16:09:55 -0400

    creating input listeners

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,50 +1,65 @@
-
-
 let WINDOW_WIDTH = window.innerWidth;
 let WINDOW_HEIGHT = window.innerHeight;
 
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
 canvas.width = WINDOW_WIDTH;
 canvas.height = WINDOW_HEIGHT;
-
 
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
 
-  return Math.floor(Math.random() * (max-min)) + min;
-}
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 
 const moveXWithinBounds = (x, size) => {
-  if(x < size) {
-  // element is overflowing off-screen to the left 
+  if (x < size) {
+    // element is overflowing off-screen to the left
     return size;
-  } else if(x > WINDOW_WIDTH - size) {
-  // element is overflowing off-screen to the right
+  } else if (x > WINDOW_WIDTH - size) {
+    // element is overflowing off-screen to the right
     return WINDOW_WIDTH - size;
-  }
-  else {
+  } else {
     return x;
   }
-}
+};
 
-const moveYWithinBounds = (y, size) => {
-  if(y < size) {
-    // element is overflowing off-screen to the left 
-    return size;
-  } else if(y > WINDOW_HEIGHT - size) {
-    // element is overflowing off-screen to the right
-    return WINDOW_HEIGHT - size;
+const moveXAwayFromMousePos = (x, size) => {
+  if (Math.abs(x - mouse.x) < size) {
+    // too close to mouse position - move x
+    return x + size;
+  } else {
+    // x is not anywhere near mouse
+    return x;
   }
-  else {
+};
+
+const moveYAwayFromMousePos = (y, size) => {
+  if (Math.abs(y - mouse.y) < size) {
+    // too close to mouse position - move x
+    return y + size;
+  } else {
+    // x is not anywhere near mouse
     return y;
   }
-}
+};
+
+const moveYWithinBounds = (y, size) => {
+  if (y < size) {
+    // element is overflowing off-screen to the left
+    return size;
+  } else if (y > WINDOW_HEIGHT - size) {
+    // element is overflowing off-screen to the right
+    return WINDOW_HEIGHT - size;
+  } else {
+    return y;
+  }
+};
 
 const drawBoundedRandomPosCircles = (amountCircles, sizeMin, sizeMax) => {
-  for(let i = 0; i < amountCircles; i++) {
+  for (let i = 0; i < amountCircles; i++) {
     c.beginPath();
 
     let size = getRandomInt(sizeMin, sizeMax);
@@ -54,10 +69,10 @@ const drawBoundedRandomPosCircles = (amountCircles, sizeMin, sizeMax) => {
     c.arc(x, y, size, 0, 2 * Math.PI, false);
     c.stroke();
   }
-}
+};
 
 const drawBoundedRandomPosSquares = (amountSquares, sizeMin, sizeMax) => {
-  for(let i = 0; i < amountSquares; i++) {
+  for (let i = 0; i < amountSquares; i++) {
     c.beginPath();
 
     let size = getRandomInt(sizeMin, sizeMax);
@@ -67,11 +82,15 @@ const drawBoundedRandomPosSquares = (amountSquares, sizeMin, sizeMax) => {
     c.fillRect(x, y, size, size);
     c.stroke();
   }
-}
+};
 
-const drawBoundedRandomBezierCurves = (numPoints, drawPoints = false, connected = false) => {
+const drawBoundedRandomBezierCurves = (
+  numPoints,
+  drawPoints = false,
+  connected = false
+) => {
   let lastEndPoint;
-  for(let i = 0; i < numPoints; i++) {
+  for (let i = 0; i < numPoints; i++) {
     c.beginPath();
 
     let startPoint = lastEndPoint || {
@@ -104,58 +123,111 @@ const drawBoundedRandomBezierCurves = (numPoints, drawPoints = false, connected 
       endPoint.y
     );
 
-    if(drawPoints) {
-      c.fillStyle = 'blue';
+    if (drawPoints) {
+      c.fillStyle = "blue";
       // start point
       c.fillRect(startPoint.x, startPoint.y, 10, 10);
       // end point
       c.fillRect(endPoint.x, endPoint.y, 10, 10);
 
-      c.fillStyle = 'red';
+      c.fillStyle = "red";
       // control points
       c.fillRect(controlPoint1.x, controlPoint1.y, 10, 10);
       c.fillRect(controlPoint2.x, controlPoint2.y, 10, 10);
-      
-
     }
-    
+
     c.stroke();
   }
-}
+};
+
+/*****************************
+ **		input listeners
+ ******************************/
 
 let mouse = {
   x: undefined,
   y: undefined
 };
-document.addEventListener('mousemove', (e) => {
+document.addEventListener("mousemove", e => {
   // console.log(e);
   mouse.x = e.x;
   mouse.y = e.y;
 });
 
-function Circle(x, y, dx, dy, radius) {
+document.addEventListener("mousedown", e => {
+  console.log("we clickin now boys");
+});
+
+/*****************************/
+
+function Marker(x, y, dx, dy, radius, opacity, color) {
   this.x = x;
   this.y = y;
   this.dx = dx;
   this.dy = dy;
   this.radius = radius;
   this.origRadius = radius;
+  this.opacity = opacity;
+  this.color = color;
 
   this.draw = function() {
+    // c.globalAlpha = this.opacity;
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-    c.strokeStyle = 'purple';
-    c.fillStyle = 'purple';
+    c.strokeStyle = this.color;
+    c.fillStyle = this.color;
     c.fill();
     c.stroke();
-  }
+  };
 
   this.update = function() {
-    if(this.x + this.radius > WINDOW_WIDTH || this.x < this.radius) {
+    this.x = mouse.x;
+    this.y = mouse.y;
+
+    this.draw();
+  };
+}
+
+function Circle(x, y, dx, dy, radius, opacity, color) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  this.origRadius = radius;
+  this.opacity = opacity;
+  this.color = color;
+
+  this.draw = function() {
+    // c.globalAlpha = this.opacity;
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+    c.strokeStyle = this.color;
+    c.fillStyle = this.color;
+    c.fill();
+    c.stroke();
+  };
+
+  this.update = function() {
+    //check if circle is within range of mouse
+    let xMin = this.x - 20;
+    let xMax = this.x + 20;
+    let yMin = this.y - 20;
+    let yMax = this.y + 20;
+
+    if (mouse.x > xMin && mouse.x < xMax && mouse.y > yMin && mouse.y < yMax) {
+      // we've hit a circle
+      //restart game
+      console.log("hit!");
+      arrayOfCircles = [];
+      initGame(arrayOfCircles);
+    }
+
+    if (this.x + this.radius > WINDOW_WIDTH || this.x < this.radius) {
       this.dx = -this.dx;
     }
 
-    if(this.y + this.radius > WINDOW_HEIGHT || this.y < this.radius) {
+    if (this.y + this.radius > WINDOW_HEIGHT || this.y < this.radius) {
       this.dy = -this.dy;
     }
 
@@ -164,59 +236,76 @@ function Circle(x, y, dx, dy, radius) {
 
     let xDistanceElToMouse = Math.abs(this.x - mouse.x);
 
-    if(xDistanceElToMouse < 50) {
-    // distance from this element to mouse is < 50
-      if(this.radius < this.origRadius + 35) {
+    if (xDistanceElToMouse < 50) {
+      // distance from this element to mouse is < 50
+      if (this.radius < this.origRadius + 35) {
         this.radius += 0.5;
       }
-    } else if(this.radius > this.origRadius) {
-
+    } else if (this.radius > this.origRadius) {
       this.radius -= 1;
     }
 
     this.draw();
-  }
+  };
 }
 
-const createArrayOfCircles = (numCircles) => {
+const createArrayOfCircles = numCircles => {
   let arrayOfCircles = [];
 
-  
-  for(let i = 0; i < numCircles; i++) {
+  for (let i = 0; i < numCircles; i++) {
     let radius = getRandomInt(5, 30);
     let x = moveXWithinBounds(getRandomInt(0, WINDOW_WIDTH), radius);
     let y = moveYWithinBounds(getRandomInt(0, WINDOW_HEIGHT), radius);
     let dx = getRandomInt(1, 3);
     let dy = getRandomInt(1, 3);
-    arrayOfCircles.push(new Circle(x, y, dx, dy, radius));
+    let opacity = Math.random();
+    arrayOfCircles.push(new Circle(x, y, dx, dy, radius, opacity, "purple"));
   }
 
   return arrayOfCircles;
-}
+};
+
+let setIntervalRef;
+
+const initGame = arrayOfCircles => {
+  console.log("initGame running");
+  clearInterval(setIntervalRef);
+  setIntervalRef = setInterval(() => {
+    let radius = getRandomInt(5, 30);
+    let x = moveXAwayFromMousePos(
+      moveXWithinBounds(getRandomInt(0, WINDOW_WIDTH), radius),
+      100
+    );
+    let y = moveYAwayFromMousePos(
+      moveYWithinBounds(getRandomInt(0, WINDOW_HEIGHT), radius),
+      100
+    );
+    let dx = getRandomInt(1, 3);
+    let dy = getRandomInt(1, 3);
+    let opacity = Math.random();
+    arrayOfCircles.push(new Circle(x, y, dx, dy, radius, opacity, "purple"));
+  }, 200);
+};
+
+let arrayOfCircles;
 
 // start
 (function() {
-
-  // drawBoundedRandomPosCircles(50, 2, 10);
-  // drawBoundedRandomPosSquares(100, 2, 60);
-  // drawBoundedRandomBezierCurves(10, false, false);
-
-  
-
-  let arrayOfCircles = createArrayOfCircles(50);
+  arrayOfCircles = []; //createArrayOfCircles(500);
+  let marker = new Marker(50, 50, 0, 0, 20, 1, "black");
 
   function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-    for(let i = 0; i < arrayOfCircles.length; i++) {
+    for (let i = 0; i < arrayOfCircles.length; i++) {
       arrayOfCircles[i].update();
     }
 
-    // circle.draw();
-    // circle.update();
-    
+    marker.update();
   }
+
+  initGame(arrayOfCircles);
 
   animate();
 })();
